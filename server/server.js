f2a3057cbb5a57b331d4b0a39b2846b83bfb5688
sabrina.moraru <sabrina.moraru@gmail.com>
2018-11-06 10:37:14 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var { ObjectID } = require("mongodb");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -66,21 +67,22 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-app.delete("/todos/:id", (req, res) => {
-  // get the id
+app.patch("/todos/:id", (req, res) => {
   var id = req.params.id;
+  var body = _.pick(req, body, ["text", "completed"]);
 
-  //validate the id -> not valid? return 404
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  // remove todo by id
-  // success
-  // if no doc, send 404
-  // if doc, send doc back with 200
-  // error
-  // 400 with empty body
-  Todo.findByIdAndRemove(id)
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
     .then(todo => {
       if (!todo) {
         return res.status(404).send();
@@ -92,6 +94,33 @@ app.delete("/todos/:id", (req, res) => {
       res.status(400).send();
     });
 });
+
+// app.delete("/todos/:id", (req, res) => {
+// get the id
+// var id = req.params.id;
+
+//validate the id -> not valid? return 404
+// if (!ObjectID.isValid(id)) {
+//   return res.status(404).send();
+// }
+// remove todo by id
+// success
+// if no doc, send 404
+// if doc, send doc back with 200
+// error
+// 400 with empty body
+//   Todo.findByIdAndRemove(id)
+//     .then(todo => {
+//       if (!todo) {
+//         return res.status(404).send();
+//       }
+
+//       res.send({ todo });
+//     })
+//     .catch(e => {
+//       res.status(400).send();
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
